e4034c875af7c559cfe68060249584ddce7d5e62
hyunwoong <wjh1@boneis.com>
2020-07-14 17:44:30 +0900

    second commit

--- src/main/java/com/woong/study/domain/posts/Posts.java
+++ src/main/java/com/woong/study/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.woong.study.domain.posts;
 
+import com.woong.study.domain.BaseTimeEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,5 +32,10 @@ public class Posts {
     this.title = title;
     this.content = content;
     this.author = author;
+  }
+
+  public void update(String title, String content) {
+    this.title = title;
+    this.content = content;
   }
 }
